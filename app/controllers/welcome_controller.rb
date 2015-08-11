3582785ed679573dd979f4a774a070d0f3afe660
buckyyy <pbsaunde@gmail.com>
2015-08-10 20:33:03 -0400

    first commit

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,14 @@
+class WelcomeController < ApplicationController
+  def index
+  	@homeland = "Boston"
+  	@countries = ["Miami", "Scotland", "San Jose", "Dallas"]
+  	@images = ["boston.jpg", "miami.jpg", "scotland.jpg", "sanjose.jpg"]
+  end
+
+  def about
+  	@color = params[:color]
+  	@height = params[:size].to_f
+  end
+end
+
+
